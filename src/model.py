f61e956c595196aadaa8c01c70d4977896ee791d
chenjy729 <chenjy729@mail2.sysu.edu.cn>
2022-12-19 21:52:35 +0800

    feat: copy some code for basic framework

---
+++ src/model.py
@@ -0,0 +1,95 @@
+from typing import List
+
+import torch
+import torch.nn as nn
+
+
+def repackage_hidden(h):
+    if isinstance(h, torch.Tensor):
+        return h.detach()
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, word2id: dict, id2word: dict, texts: List[List[str]]) -> None:
+        super().__init__()
+        self.word2id = word2id
+        self.id2word = id2word
+        self.texts = [self.get_ids_by_words(text) for text in texts]
+
+    def get_words_by_ids(self, ids: List[int]) -> str:
+        return ' '.join([self.id2word[id] for id in ids])
+
+    def get_ids_by_words(self, words: List[str]) -> List[int]:
+        # 将未登陆词标记为 <unk>
+        return [self.word2id[word if word in self.word2id else '<unk>'] for word in words]
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        sent = self.texts[idx]
+        target = sent[1:] + [0]
+        sent = torch.tensor(sent)
+        target = torch.tensor(target)
+        return sent, target
+
+
+class RNNModel(nn.Module):
+    def __init__(self, rnn_type, ntoken, ninp, nhid, dropout=0.5):
+        super(RNNModel, self).__init__()
+
+        self.rnn_type = rnn_type
+        self.vocab_size = ntoken
+        self.nhid = nhid
+
+        self.drop = nn.Dropout(dropout)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        self.decoder = nn.Linear(nhid, ntoken)
+
+        if rnn_type in ['LSTM', 'GRU']:
+            self.rnn = getattr(nn, rnn_type)(
+                ninp, nhid, dropout=dropout, batch_first=True)
+        else:
+            try:
+                nonlinearity = {'RNN_TANH': 'tanh',
+                                'RNN_RELU': 'relu'}[rnn_type]
+            except KeyError:
+                raise ValueError("unknown parameter, you can use \
+                    ['LSTM', 'GRU', 'RNN_TANH', 'RNN_RELU']")
+            self.rnn = nn.RNN(ninp, nhid, nonlinearity=nonlinearity,
+                              dropout=dropout, batch_first=True)
+
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.1
+        # 词嵌入模型的权值初始化
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+
+        # 线性转化器的权值和偏移值大小
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    # critical：定义前向传播
+    def forward(self, inputs, hidden):
+        emb = self.drop(self.encoder(inputs))
+        # output：每一次输入的输出
+        # hidden：最后一次的输出，会被decoder直接使用
+        output, hidden = self.rnn(emb)
+        output = self.drop(output)
+        # critical：然后进行线性转换，view函数的用处是改变tensor的形状
+        # 因为我们要训练，所以对每个位置的字词都要预测一次，所以使用包含所有字词的output
+        decoded = self.decoder(output.view(
+            output.size(0) * output.size(1), output.size(2)))
+
+        return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
+
+    def init_hidden(self, bsz, requires_grad=True):
+        weight = next(self.parameters())
+        if self.rnn_type == 'LSTM':
+            return (weight.new_zeros((1, bsz, self.nhid), requires_grad=requires_grad),
+                    weight.new_zeros((1, bsz, self.nhid), requires_grad=requires_grad))
+        else:
+            return weight.new_zeros((1, bsz, self.nhid), requires_grad=requires_grad)
---
+++ src/train.py
@@ -0,0 +1,146 @@
+import json
+import random
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+import model as M
+import utils
+
+USE_CUDA = torch.cuda.is_available()
+
+# 设置随机种子
+SEED = 202212
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+if USE_CUDA:
+    torch.cuda.manual_seed(SEED)
+    torch.cuda.set_device(1)
+
+# 超参设置
+BATCH_SIZE = 32
+EMBEDDING_SIZE = 128
+# MAX_VOCAB_SIZE = 50000
+GRAD_CLIP = 1
+NUM_EPOCHS = 100
+
+LOSS_FN = nn.CrossEntropyLoss()
+LEARNING_RATE = 0.001
+
+
+lines, words = utils.read_data('train_en.txt')
+word2id, id2word = utils.make_map(words)
+VOCAB_SIZE = len(word2id)
+with open('word2id.json', 'w') as f:
+    json.dump(word2id, f)
+with open('id2word.json', 'w') as f:
+    json.dump(id2word, f)
+ds_train = M.Dataset(word2id, id2word, lines)
+dl_train = DataLoader(ds_train)
+
+lines_e, words_e = utils.read_data('eval_en.txt')
+ds_eval = M.Dataset(word2id, id2word, lines_e)
+dl_eval = DataLoader(ds_eval)
+
+model = M.RNNModel('RNN_TANH', VOCAB_SIZE, EMBEDDING_SIZE,
+                   nhid=512, dropout=0.5)
+if USE_CUDA:
+    model = model.cuda()
+
+
+# critical：模型评估，建议先跳过这部分最后再看
+def evaluate(model, dataloader):
+    # 进入评估状态
+    model.eval()
+    total_loss = 0
+    total_count = 0
+
+    # 不是训练，关闭梯度加快运行速度
+    with torch.no_grad():
+        hidden = model.init_hidden(BATCH_SIZE, requires_grad=False)
+        # 将数据按batch输入
+        for i, batch in enumerate(dataloader):
+            data, target = batch
+            if USE_CUDA:
+                data, target = data.cuda(), target.cuda()
+
+            hidden = M.repackage_hidden(hidden)
+
+            with torch.no_grad():
+                output, hidden = model(data, hidden)
+
+                # model(data,hidden) 相当于调用model.forward
+
+            # 计算损失
+            loss = LOSS_FN(output.view(-1, VOCAB_SIZE), target.view(-1))
+
+            total_count += np.multiply(*data.size())
+
+            total_loss += loss.item() * np.multiply(*data.size())
+
+        loss = total_loss / total_count
+        model.train()
+
+        return loss
+
+
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+# critical：训练的主要流程
+val_losses = []
+for epoch in range(NUM_EPOCHS):
+    # critical：训练前务必手动设置model.train()
+    # model.train()对应另一个函数model.eval(),前者有梯度用于训练，后者无梯度节省内存用于测试
+    # 神经网络的后向传播和更新都依赖于梯度，没有梯度跑几个epoch都是无济于事
+    model.train()
+    hidden = model.init_hidden(BATCH_SIZE)
+
+    # critical：将数据集中的数据按batch_size划分好，一一读入模型中
+    for i, batch in enumerate(dl_train):
+        data, target = batch
+
+        # 使用gpu训练需要将数据也迁移到gpu
+        if USE_CUDA:
+            data, target = data.cuda(), target.cuda()
+
+        hidden = M.repackage_hidden(hidden)
+        model.zero_grad()  # critical：每步运行之前清空前一步backward留下的梯度，否则梯度信息不准确
+
+        # print(data.size(), hidden[0].size())
+        # critical：模型的forward，将数据正式传入模型中计算并输出结果
+        # 输入：hidden：[BATCH_SIZE, seq_max_len]
+        output, hidden = model(data, hidden)
+
+        # critical：计算模型输出与真实标签的差距，也就是损失loss
+        # 需要注意，设计模型时没有必要对output进行手动softmax为概率分布
+        # nn.CrossEntropyLoss()会自动帮你完成这一步，否则二次softmax将导致模型训练不如预期
+        loss = LOSS_FN(output.view(-1, VOCAB_SIZE), target.view(-1))
+
+        # critical：梯度回传，准备更新模型参数
+        loss.backward()
+
+        # 解决梯度爆炸的问题
+        nn.utils.clip_grad_norm_(model.parameters(), GRAD_CLIP)
+
+        # critical：optimizer更新模型参数
+        optimizer.step()
+
+    # 定时打印模型损失，查看模型训练情况
+    if (epoch+1) % 5 == 0:
+        print("epoch:", epoch, " iter:", i, "loss", loss.item())
+
+    # 定时evaluate模型，查看模型训练情况
+    if (epoch+1) % 10 == 0:
+        val_loss = evaluate(model, dl_eval)
+
+        # critical：根据evaluate的结果，保存最好的模型
+        if len(val_losses) == 0 or val_loss < min(val_losses):
+            print("epoch:", epoch, "best model, val loss: ", val_loss)
+            # critical：使用torch.save()保存模型到路径lm-best.th
+            # 之后可以通过torch.load()读取保存好的模型
+            torch.save(model.state_dict(), "lm-best.th")
+
+        val_losses.append(val_loss)
